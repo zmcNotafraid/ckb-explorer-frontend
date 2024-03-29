b6b332d300239c2ff3022ea2aa5b4a6a4c9a2bc8
Miles Zhang <mingchang555@hotmail.com>
2024-03-29 17:34:45 +0800

    feat: show Nodes distribution by Country charts
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -153,3 +153,5 @@ export const MAINNET_URL = `https://${config.BASE_URL}`
 export const TESTNET_URL = `https://${ChainName.Testnet}.${config.BASE_URL}`
 
 export const TYPE_ID_CODE_HASH = '0x00000000000000000000000000000000000000000000000000545950455f4944'
+
+export const NETWORK = config.CHAIN_TYPE === 'testnet' ? 'pudge' : 'minara'
